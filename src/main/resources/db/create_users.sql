a72efb66340e9d8e1de2b83767e8c478c0b2facb
Christian Egli <christian.egli@gmx.net>
2016-05-06 18:36:37 +0200

    prepare for first release

--- src/main/resources/db/create_users.sql
+++ src/main/resources/db/create_users.sql
@@ -3,7 +3,7 @@
 
 INSERT INTO benutzer VALUES(default, 'zoe', '$2a$04$alCNYl6zQ012IcdtlcY3DuifdmXiAOQZXJY7.IgSc50YG8sLZytO2', 'athlet', 'Zoe', 'Egli', null, null, null);
 INSERT INTO benutzer VALUES(default, 'liv', '$2a$04$2etITmFEzkCZqPjgxb4JHOOUwDvTZStsjpYGM0ep0qDzrMznBSYZK', 'athlet', 'Liv', 'Egli', null, null, null);
-INSERT INTO benutzer VALUES(default, 'jojo', '$2a$04$j0rpsAv.Dz4fTm8rRVfZb.aBmQIjCPVaFrok1fTLzLKStIOa.OkcC', 'athlet', 'Joelle', 'Egli', null, null, null);
+INSERT INTO benutzer VALUES(default, 'jojo', '$2a$04$j0rpsAv.Dz4fTm8rRVfZb.aBmQIjCPVaFrok1fTLzLKStIOa.OkcC', 'athlet,admin', 'Joelle', 'Egli', null, null, null);
 INSERT INTO benutzer VALUES(default, 'chrigu', '$2a$04$rHEt7ejCf3T6LtM1OinHEepvjoKlj7lY6GVdDquYrFHdvASq8wnXO', 'athlet,admin', 'Christian', 'Egli', null, null, null);
 
 INSERT INTO benutzer VALUES(default, 'joelle', '$2a$04$h0wy./H0dMlrvbxe56Bk1.BGjdh4sQ2TZa5/lUxLOYGBQp8.kdwSa', 'athlet', 'Joelle', 'Niederberger', null, null, null);  -- 94
