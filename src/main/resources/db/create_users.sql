e8b34b0f640ccbe4e352b8480e418b37fb286e7b
Christian Egli <christian.egli@gmx.net>
2016-11-20 10:29:52 +0100

    New users added

--- src/main/resources/db/create_users.sql
+++ src/main/resources/db/create_users.sql
@@ -9,3 +9,8 @@ INSERT INTO benutzer VALUES(default, 'chrigu', '$2a$04$rHEt7ejCf3T6LtM1OinHEepvj
 INSERT INTO benutzer VALUES(default, 'joelle', '$2a$04$h0wy./H0dMlrvbxe56Bk1.BGjdh4sQ2TZa5/lUxLOYGBQp8.kdwSa', 'athlet', 'Joelle', 'Niederberger', null, null, null);  -- 94
 INSERT INTO benutzer VALUES(default, 'maex', '$2a$04$H3Q9lkcqFz3et4WC/ButjO8pl/hANxTfVD6yPfLHv7wWfr/wUyWWS', 'athlet,admin', 'Mäx', 'Grossmann', null, null, null); -- 99
 
+INSERT INTO benutzer VALUES(default, 'jonas', '$2a$04$lF7YC92u9lThGuc5dkQTa.4zT7gXR7XSyToc2d.CfbgOlCWA4.nQm', 'athlet', 'Jonas', 'Utelli', null, null, null);  -- 99
+INSERT INTO benutzer VALUES(default, 'francesca', '$2a$04$a4Kn3rpFhqqPcBuutSVTcuB9yi22SvNEjGiOdpK0sl.MxHh3/VZhG', 'athlet', 'Francesca', 'Walther', null, null, null);  -- 99
+INSERT INTO benutzer VALUES(default, 'leon', '$2a$04$Y6tOARZSIUhlI6QZzp.V9Oz3qzoiWfys/vgIW5sNvVNU8lR9PSEmC', 'athlet', 'Leon', 'Niederberger', null, null, null);  -- 99
+INSERT INTO benutzer VALUES(default, 'levin', '$2a$04$EjO5T/HaoxUI52SWVAdW8OLg1sHE4IpqXDkVUZUpHgMZOkHomNke2', 'athlet', 'Levin', 'Straubhaar', null, null, null);  -- 99
+
