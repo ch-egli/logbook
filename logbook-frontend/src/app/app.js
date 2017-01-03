781a9f731f93904abe61173ee0891b3cbabaa0ad
Christian Egli <christian.egli@gmx.net>
2017-01-03 20:57:50 +0100

    version 1.2.2

--- logbook-frontend/src/app/app.js
+++ logbook-frontend/src/app/app.js
@@ -45,7 +45,7 @@ angular.module('app', [
 
     // Globale Konfigurationeinstellungen
     .constant('config', {
-        logbookVersion: '1.2.1',
+        logbookVersion: '1.2.2',
 
         //resourceServerUrl: window.location.origin === 'http://localhost:3001' ? 'http://ec2-52-57-16-191.eu-central-1.compute.amazonaws.com:8181/' : window.location.origin + '/',
 
