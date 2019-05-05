1a12d673f2c5954b68758102d88dcbf5d53e027c
ch-egli <chrsitian.egli@gmx.net>
2019-05-05 23:02:35 +0200

    release 1.4.3 (build info angepasst)

--- logbook-frontend/src/app/app.js
+++ logbook-frontend/src/app/app.js
@@ -45,7 +45,7 @@ angular.module('app', [
 
     // Globale Konfigurationeinstellungen
     .constant('config', {
-        logbookVersion: '1.4.2',
+        logbookVersion: '1.4.3',
 
         //resourceServerUrl: window.location.origin === 'http://localhost:3001' ? 'http://ec2-52-57-16-191.eu-central-1.compute.amazonaws.com:8181/' : window.location.origin + '/',
 
